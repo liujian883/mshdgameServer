2639a03f1b4226834b0a438290040029cc89b053
wangjingxin <1021829465@qq.com>
2014-12-29 16:35:48 +0800

    批量插入数据测试

---
+++ login/DbTest.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once ('../db/db.php');
+//include_once ('../db/error.php');
+$db = DB::getInstance();
+$db->init_db(1);
+//echo " DbTest ";
+echo date('H:i:s').PHP_EOL;
+mysql_query("BEGIN");
+$newUser = array();
+
+for($i=1;$i<4000001;$i++)
+{
+     $newUser['pwd'] = "'".$i."'";
+     $newUser['userName'] = "'".$i."'";
+     //echo " $i ";
+     $db->insert2('userID',$newUser,true);
+     if($i%10==0)
+     {
+        mysql_query("COMMIT");
+        mysql_query("BEGIN");
+     }
+}
+mysql_query("COMMIT");
+echo date('H:i:s').PHP_EOL;
+
+?>
